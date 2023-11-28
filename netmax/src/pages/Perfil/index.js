019d930d2a5d6e1cd054885bfc5bd0cbcf707588
Jorge <jorge.coronado@acct.global>
2023-11-28 01:45:59 -0300

    feat: adding put req. to the ´perfil´ page

--- netmax/src/pages/Perfil/index.js
+++ netmax/src/pages/Perfil/index.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, StatusBar, Button } from "react-native";
 import apiUser from "../../services/ApiUser";
-import { useNavigation } from "@react-navigation/native"
+import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { StyleSheet } from "react-native-web"
+import { StyleSheet } from "react-native-web";
 
 export default function Perfil() {
   const [userData, setUserData] = useState(null);
-  const [nome, setNome] = useState('')
-  const [email, setEmail] = useState('')
-  const [senha, setSenha] = useState('')
-  const [dataN, setDataN] = useState()
-  const [cpf, setCpf] = useState('')
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,6 +19,9 @@ export default function Perfil() {
         const ID_CLIENT = await AsyncStorage.getItem("user");
         const response = await apiUser.get(`/${ID_CLIENT}`);
         setUserData(response.data.data);
+        setNome(userData?.nome)
+        setEmail(userData?.email)
+        setSenha(userData?.senha)
       } catch (error) {
         console.error(error);
       }
@@ -27,78 +30,96 @@ export default function Perfil() {
     fetchData();
   }, []);
 
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
-  async function handleUpdate(){
-    const response = await apiUser.post('/create', {
-        "nome":nome,
-        "email":email,
-        "cpf":cpf,
-        "dataNascimento":dataN,
-        "senha":senha
-    })
-    if(response.status == 201){
-        navigation.navigate('Login')
+  async function handleUpdate() {
+    const ID_CLIENT = await AsyncStorage.getItem("user");
+    const response = await apiUser.put(`user/${ID_CLIENT}`, {
+      nome: nome,
+      email: email,
+      cpf: userData?.cpf,
+      dataNascimento: userData?.dataNascimento,
+      senha: senha,
+    });
+
+    console.log(response)
+    if (response.status == 201) {
+      navigation.navigate("Login");
     }
-}
+  }
 
-const styles = StyleSheet.create({
-  input:{
-      height: '8%',
-      width: '90%',
-      alignSelf: 'center',
-      borderStyle: 'solid',
+  const styles = StyleSheet.create({
+    input: {
+      height: "8%",
+      width: "90%",
+      alignSelf: "center",
+      borderStyle: "solid",
       borderWidth: 1,
       borderRadius: 4,
-      marginBottom: '5%',
-      backgroundColor: "#f0f0f0"
-  },
-  text:{
-      marginStart: '5%',
-      color: "#f0f0f0"
-
-  },
-  container:{
+      marginBottom: "5%",
+      backgroundColor: "#f0f0f0",
+    },
+    disabledInput:{
+      height: "8%",
+      width: "90%",
+      alignSelf: "center",
+      borderStyle: "solid",
+      borderWidth: 1,
+      borderRadius: 4,
+      marginBottom: "5%",
+      backgroundColor: "#41414d",
+      color: "white"
+    },
+    text: {
+      marginStart: "5%",
+      color: "#f0f0f0",
+    },
+    container: {
       backgroundColor: "#151517",
       height: "100%",
-  },
-  })
+    },
+  });
 
   return (
     <>
-      <StatusBar/>
-      <View>
+      <StatusBar />
+      <View style={styles.container}>
         {userData && (
-          <>
-            <Text>Nome:</Text>
-            <TextInput value={userData.nome} />
-            <Text>Email:</Text>
-            <TextInput value={userData.email} />
-            <Text>Senha:</Text>
-            <TextInput value={userData.senha} />
-            <Text>Data de Nascimento: {userData.DataNascimento}</Text>
-            <Text>CPF: {userData.cpf}</Text>
-          </>
+          <View style={styles.card}>
+            <Text style={styles.text}>Nome:</Text>
+            <TextInput
+              style={styles.input}
+              value={nome}
+              onChangeText={setNome}
+            />
+            <Text style={styles.text}>Email:</Text>
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+            />
+            <Text style={styles.text}>CPF:</Text>
+            <TextInput
+              style={styles.disabledInput}
+              value={userData.cpf}
+              editable={false}
+            />
+            <Text style={styles.text}>Data de nascimento:</Text>
+            <TextInput
+              style={styles.disabledInput}
+              placeholder={userData.dataNascimento}
+              editable={false}
+            />
+            <Text style={styles.text}>Senha:</Text>
+            <TextInput
+              style={styles.input}
+              value={senha}
+              onChangeText={setSenha}
+            />
+            <Button title="Criar" onPress={() => handleUpdate()} />
+          </View>
         )}
       </View>
-
-      <View style={styles.container}>
-            <View  style={styles.card}>
-                <Text style={styles.text}>Nome:</Text>
-                <TextInput style={styles.input} value={userData.nome} onChangeText={setNome}/>
-                <Text style={styles.text}>Email:</Text>
-                <TextInput style={styles.input} value={userData.email} onChangeText={setEmail}/>
-                <Text style={styles.text}>CPF:</Text>
-                <TextInput style={styles.input} value={userData.cpf} onChangeText={setCpf}/>
-                <Text style={styles.text}>Data de nascimento:</Text>
-                <TextInput placeholder="YYYY/MM/DD" style={styles.input} value={userData.dataNascimento} onChangeText={setDataN}/>
-                <Text style={styles.text}>Senha:</Text>
-                <TextInput style={styles.input} value={userData.senha} onChangeText={setSenha}/>
-                <Button title='Criar' onPress={
-                    ()=>handleUpdate()
-                } />
-            </View>
-        </View>
     </>
   );
 }
